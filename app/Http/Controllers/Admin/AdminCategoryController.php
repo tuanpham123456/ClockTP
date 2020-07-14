452b1acf0e01488a1fdb147313f4b69c7f340c4f
admin <bicudu0403@gmail.com>
2020-07-14 19:51:08 +0700

    CẬP NHẬT,XÓA, ẨN HIỆN ACTIVE,HOT,GẮN LINK ROUTE +      CONTROLLER: -   AdminCategoryController: - edit - delete: xóa - active: ẩn hiện - hot: ẩn hiện

--- app/Http/Controllers/Admin/AdminCategoryController.php
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -12,7 +12,11 @@ use Illuminate\Http\Request;
 class AdminCategoryController extends AdminController
 {
     public function index(){
-        return view ('admin.category.index');
+        $categories     =   Category::paginate(10);
+        $viewData       =[
+            'categories'    => $categories
+        ];
+        return view ('admin.category.index',$viewData);
     }
     public function create(){
         return view ('admin.category.create');
@@ -26,4 +30,41 @@ class AdminCategoryController extends AdminController
 
         return redirect()->back();
     }
+    public function edit($id){
+        $category   =   Category::find($id);
+        return view ('admin.category.update', compact('category'));
+    }
+
+    public function update(AdminRequestCategory $request, $id){
+        $category   =   Category::find($id);
+        $data       =   $request->except('_token');
+        $data['c_slug']     =   Str::slug($request->c_name);
+        $data['updated_at'] =   Carbon::now();
+
+        $category->update($data);
+        return redirect()->back();
+    }
+    public function delete($id){
+        $category   =   Category::find($id);
+        if ($category) $category->delete();
+
+        return redirect()->back();
+    }
+
+    public function active($id){
+        $category = Category::find($id);
+        $category->c_status = ! $category->c_status;
+        $category->save();
+        return redirect()->back();
+
+    }
+
+    public function hot($id){
+        $category = Category::find($id);
+        $category->c_hot = ! $category->c_hot;
+        $category->save();
+        return redirect()->back();
+
+    }
+
 }
